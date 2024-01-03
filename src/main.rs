70447a849b16eaf1bee5576a6364a87560869d26
Ben Eggers <ben@beneggers.com>
2024-01-03 14:27:51 -0800

    Crappy CLI tool to interact with it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+mod idx;
+
+use idx::PositionalInvertedIndex;
+use clap::{App, Arg, SubCommand};
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Positional Inverted Index CLI")
+        .version("0.1")
+        .author("beggers")
+        .about("Manages a positional inverted index")
+        .arg(Arg::with_name("INDEX")
+            .help("Sets the path to the index file")
+            .required(true)
+            .index(1))
+        .subcommand(SubCommand::with_name("index")
+            .about("Indexes a document")
+            .arg(Arg::with_name("DOC_ID")
+                .help("The ID of the document to index")
+                .required(true))
+            .arg(Arg::with_name("CONTENT")
+                .help("The content of the document to index")
+                .required(true)))
+        .subcommand(SubCommand::with_name("search")
+            .about("Searches the index")
+            .arg(Arg::with_name("QUERY")
+                .help("The query string to search for")
+                .required(true)))
+        .get_matches();
+
+    let index_path = matches.value_of("INDEX").unwrap();
+
+    let mut index = if Path::new(index_path).exists() {
+        let data = fs::read_to_string(index_path).expect("Unable to read file");
+        serde_json::from_str(&data).expect("Unable to parse file")
+    } else {
+        PositionalInvertedIndex::new()
+    };
+
+    match matches.subcommand() {
+        ("index", Some(sub_m)) => {
+            let doc_id = sub_m.value_of("DOC_ID").unwrap().parse::<usize>().expect("Invalid document ID");
+            let content = sub_m.value_of("CONTENT").unwrap();
+            index.index_document(doc_id, content);
+
+            let serialized = serde_json::to_string(&index).expect("Unable to serialize index");
+            fs::write(index_path, serialized).expect("Unable to write file");
+        },
+        ("search", Some(sub_m)) => {
+            let query = sub_m.value_of("QUERY").unwrap();
+            let results = index.search(query);
+            println!("Search results: {:?}", results);
+        },
+        _ => panic!("You must specify a subcommand: either 'index' or 'search'"),
+    }
 }
