315b5e8a966bd1da173e67f6edd2b02a335d85ce
Ben Eggers <ben@beneggers.com>
2024-01-04 15:45:25 -0800

    plont

--- src/plot.rs
+++ src/plot.rs
@@ -1,7 +1,6 @@
 use plotters::prelude::*;
 use std::{
     error::Error,
-    f64,
     path::Path
 };
 
@@ -10,27 +9,30 @@ pub fn plot_indexing_duration(target_dir: &str) -> Result<(), Box<dyn Error>> {
     let output_path = Path::new(target_dir).join("indexing_data.png");
 
     let mut rdr = csv::Reader::from_path(input_path)?;
-    let (data, max_indexing_duration) = rdr
-        .records()
-        .filter_map(Result::ok)
-        .fold((Vec::new(), 0f64), |(mut data, max_duration), record| {
-            let document_count: i32 = record[0].parse().expect("Failed to read document count!");
-            let indexing_duration: f64 = record[1].parse().unwrap();
+    let mut data = Vec::new();
+    let mut max_indexing_duration = 0u128;
+    let mut max_documents = 0i32;
 
-            data.push((document_count, indexing_duration));
-            (data, max_duration.max(indexing_duration))
-        });
+    for result in rdr.records() {
+        let record = result?;
+        let document_count: i32 = record[0].parse().expect("Failed to read document count!");
+        let indexing_duration: u128 = record[1].parse().unwrap();
 
-    let y_axis_upper_bound = max_indexing_duration * 1.5;
+        data.push((document_count, indexing_duration));
+        max_indexing_duration = max_indexing_duration.max(indexing_duration);
+        max_documents = document_count;
+    }
 
-    let root = BitMapBackend::new(&output_path, (640, 480)).into_drawing_area();
+    let y_axis_upper_bound = max_indexing_duration;
+
+    let root = BitMapBackend::new(&output_path, (1280, 960)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
         .caption("Document Count vs Indexing Duration", ("sans-serif", 20))
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(0..1000, 0f64..y_axis_upper_bound)?;
+        .build_cartesian_2d(0..max_documents, 0u128..y_axis_upper_bound)?;
 
     chart.configure_mesh().draw()?;
     chart.draw_series(LineSeries::new(data, &RED))?;
